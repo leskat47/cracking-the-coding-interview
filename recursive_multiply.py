d15edfdd176c725d3ba7e1972be5e5eaa4331d88
Leslie <leslie@hackbrightacademy.com>
2017-09-05 22:55:01 +0000

    Update variable names

--- recursive_multiply.py
+++ recursive_multiply.py
@@ -22,38 +22,38 @@ def mult2(x, y):
     return mult_helper(smaller, bigger, memo)
 
 
-def mult_helper(smaller, bigger, memo):
-    """ Return sum of bigger value smaller number of times """
+def mult_helper(multiplier, num_to_multiply, memo):
+    """ Return sum of num_to_multiply value smaller number of times """
 
     # The following could be written in a more concise way, but was intended to
     # be more step-wise and explanatory for presentation.
 
     # Base cases:
-    if smaller == 0:
+    if multiplier == 0:
         return 0
-    if smaller == 1:
-        return bigger
+    if multiplier == 1:
+        return num_to_multiply
 
-    # If we've already solved for smaller, return value from memo.
-    if memo.get(smaller):
-        return memo[smaller]
+    # Check our cache: if we've already solved for multiplier, return value from memo.
+    if memo.get(multiplier):
+        return memo[multiplier]
 
-    # Break smaller in two. 
-    first_half = smaller / 2
-    second_half = smaller - first_half
+    # Break multiplier in two. It may be uneven
+    first_half = multiplier / 2
+    second_half = multiplier - first_half
 
     # Recurse on first half
-    first_mult = mult_helper(first_half, bigger, memo)
+    first_mult = mult_helper(first_half, num_to_multiply, memo)
 
     # If the halves were not equal recurse second half,
     # else second recursion is same as first
     if first_half != second_half:
-        second_mult = mult_helper(second_half, bigger, memo)
+        second_mult = mult_helper(second_half, num_to_multiply, memo)
     else:
         second_mult = first_mult
 
-    # Add smaller to memo dictionary with result as the value
-    memo[smaller] = first_mult + second_mult
+    # Update cache with our result
+    memo[multiplier] = first_mult + second_mult
 
     # Return result
-    return memo[smaller]
+    return memo[multiplier]
