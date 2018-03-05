f2f51ea87b7ccd4d2ec4215c07361218e03269a2
leskat47 <lesliekate@gmail.com>
2018-03-04 19:22:55 -0800

    Make solution more efficient

--- recursion/parens.py
+++ recursion/parens.py
@@ -21,7 +21,34 @@ def find_combs(n, stack , p):
     # try adding open and adding close
     if stack and n:
         find_combs(n, stack[:-1], p + ')')
+
         find_combs(n-1, stack + ['('], p + '(')
 
 
-print_parens(3)
+def print_parens1(n):
+    find_combos(n-1, 1, '(')
+
+
+def find_combos(n, stack , p):
+
+    # base case: empty stack and n is zero
+    if not stack and not n:
+        print p
+        return
+
+    # if we have reached n open parens add a close paren
+    if stack and not n:
+        find_combos(n, stack - 1, p + ')')
+
+    # if stack is empty, current parens are balanced,
+    # if we haven't reached n, we must open
+    if not stack and n:
+        find_combos(n - 1, stack + 1, p + '(')
+
+    # if stack isn't empty, parens are unbalanced
+    # try adding open and adding close
+    if stack and n:
+        find_combos(n, stack - 1, p + ')')
+        find_combos(n - 1, stack + 1, p + '(')
+
+print_parens1(3)
