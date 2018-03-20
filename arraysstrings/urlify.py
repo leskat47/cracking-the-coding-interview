7cb0e2a7363a4b73690dc2374af87feb88a01adb
leskat47 <lesliekate@gmail.com>
2018-03-19 18:40:33 -0700

    Add second solution

--- arraysstrings/urlify.py
+++ arraysstrings/urlify.py
@@ -25,3 +25,30 @@ def URLify(s_arr):
             else:
                 start = end + 1
                 end += 1
+
+def URLify_2(s_arr):
+    """ Given a string replace blank spaces with %20 (in place, so use a list)
+
+    >>> s_arr = ['a', ' ', ' ', 'b', ' ', 'c' , ' ']
+    >>> URLify_2(s_arr)
+    >>> s_arr
+    ['a', '%20', 'b', '%20', 'c']
+
+    """
+
+    left = len(s_arr) - 1
+    right = len(s_arr) - 1
+
+    for left in range(len(s_arr) - 1, -1, -1):
+        if s_arr[left] == ' ':
+            if right == len(s_arr) - 1:
+                s_arr.pop()
+                right -= 1
+            if left == 0:
+                s_arr[left:right + 1] = []
+        else:
+            if left != right:
+                s_arr[left + 1:right + 1] = ['%20']
+                right = left - 1
+            else:
+                right = right - 1
